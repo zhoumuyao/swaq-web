56a0e05a42f755a9c5c42b770a312e4ece5b7898
abttt0603 <1301273560@qq.com>
2023-11-29 14:40:21 +0800

    handle页面拆分为多个vue

---
+++ src/views/handle/feedback.vue
@@ -0,0 +1,157 @@
+<template>
+  <div class="app">
+    <!--    <sidebar></sidebar>-->
+    <div class="content">
+      <router-view></router-view>
+      <div>
+        <!--        现场处置模块-->
+        <el-steps :active="active" finish-status="success" align-center style="margin-top: 20px">
+          <el-step title="基本信息上传" ></el-step>
+          <el-step title="生物危险因子信息"></el-step>
+          <el-step title="处置决策结果"></el-step>
+          <el-step title="评价与反馈"></el-step>
+        </el-steps>
+      </div>
+
+      <div v-if="active === 3" class="center-container">
+        <el-card class="card_box">
+          <div style="margin-left: 50px;margin-top: 50px">
+            <div>现场处置评分</div>
+            <div>
+              <el-rate
+                  v-model="Starvalue"
+                  :texts="texts"
+                  show-text>
+              </el-rate>
+            </div>
+          </div>
+          <div style="margin: 50px 50px 0 50px">
+            <el-input
+                type="textarea"
+                :autosize="{ minRows: 8, maxRows: 15}"
+                placeholder="请输入您对本模块的反馈意见"
+                v-model="textarea">
+            </el-input>
+          </div>
+          <div style="float: right;margin-top: 50px;margin-right: 50px"><el-button type="primary" @click="submit">提交</el-button></div>
+        </el-card>
+      </div>
+      <el-alert
+          v-if="alertVisible && active === 3"
+          title="提交成功"
+          type="success"
+          description="感谢您的反馈意见，我们将根据反馈进行进一步优化！"
+          show-icon
+          @close="closeAlert">
+      </el-alert>
+
+      <!-- 切换页面-->
+      <router-link :to="{path: '/handle1'}">
+        <el-button class="next-button" size="large">
+          下一步
+        </el-button>
+      </router-link>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+import { get } from "@/net";
+import { ElMessage } from "element-plus";
+import { useRouter } from "vue-router";
+import Sidebar from '@/components/sideBar/SideBar.vue';
+
+// 当前步骤
+const active = ref(3);
+const Starvalue = ref(0);
+const texts = ref(['完全没有帮助','几乎没有帮助','有一点参考价值','较好参考价值','非常具有参考价值']);
+const textarea = ref('')
+const alertVisible = ref(false)
+
+const submit = () => {
+  if(textarea.value !== '' && Starvalue.value > 0){
+    alertVisible.value = true;
+  }
+}
+//
+const closeAlert= () => {
+  alertVisible.value = false;
+}
+
+</script>
+
+<style scoped>
+.app {
+  display: flex;
+  height: 100vh;
+}
+
+.content {
+  flex: 1;
+  margin-left: 200px; /* 与侧边栏宽度对应 */
+  /* 添加其他样式，如内容区域的填充等 */
+}
+.next-button {
+  position: fixed;
+  bottom: 20px;
+  right: 60px;
+}
+.upload-demo {
+  width: 400%;
+  height: 200%;
+}
+.center-container{
+  position: fixed;
+  top: 100px;
+  width: 89%;
+  height: 100%;
+}
+
+.card_box{
+  height:100%;
+  overflow-y:auto;
+  overflow-x:hidden;
+}
+
+.box-card{
+  height: 100%;
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+.left-top-container{
+  position: fixed;
+  top: 20%;
+  left: 21%;
+  width: 50%;
+  height: 35%;
+}
+.left-bottom-container{
+  position: fixed;
+  top: 60%;
+  left: 21%;
+  width: 50%;
+  height: 35%;
+}
+
+.right-container{
+  position: fixed;
+  top: 20%;
+  right: 5%;
+  width: 20%;
+  height: 65%;
+}
+
+.card-title{
+  font-size: larger;
+}
+
+.scrollbar-wrapper {
+  margin-left: 360px;
+  margin-top: 50px;
+  width: 30%;
+}
+
+</style>
