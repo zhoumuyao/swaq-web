0c82e57f9d37ac1725d70c32e8ff23f6790cdc94
1159911026@qq.com <1159911026@qq.com>
2023-12-07 13:45:16 +0800

    修改风险分析和风险评价

--- src/views/risk/risk2.vue
+++ src/views/risk/risk2.vue
@@ -24,28 +24,34 @@
               <div slot="header" class="jugehappentop">
                 <span>风险发生的可能性分析</span>
               </div>
-              <el-input class="inputtype" type="textarea" :autosize="{ minRows: 6, maxRows: 10}" placeholder="请输入内容" v-model="textarea1"></el-input>
+              <!-- <el-input class="inputtype" type="textarea" :autosize="{ minRows: 6, maxRows: 10}" placeholder="请输入内容" v-model="textarea1"></el-input> -->
+              <div v-for="(text, index) in text1" :key="index" style="margin-top: 15px">{{ index + 1 }}、{{ text }}</div>
             </el-card>
 
             <el-card class="field_hazard">
               <div slot="header" class="field_hazardtop">
                 <span>现场勘查工作产生危害分析</span>
               </div>
-              <el-input class="inputtype" type="textarea" :autosize="{ minRows: 6, maxRows: 10}" placeholder="请输入内容" v-model="textarea2"></el-input>
+              <!-- <el-input class="inputtype" type="textarea" :autosize="{ minRows: 6, maxRows: 10}" placeholder="请输入内容" v-model="textarea2"></el-input>
+               -->
+               <div v-for="(text, index) in text2" :key="index" style="margin-top: 15px">{{ index + 1 }}、{{ text }}</div>
             </el-card>
 
             <el-card class="jugehappen">
               <div slot="header" class="jugehappentop">
                 <span>附近居民生命健康影响分析</span>
               </div>
-              <el-input class="inputtype" type="textarea" :autosize="{ minRows: 6, maxRows: 10}" placeholder="请输入内容" v-model="textarea3"></el-input>
+              <!-- <el-input class="inputtype" type="textarea" :autosize="{ minRows: 6, maxRows: 10}" placeholder="请输入内容" v-model="textarea3"></el-input> -->
+              <div v-for="(text, index) in text3" :key="index" style="margin-top: 15px">{{ index + 1 }}、{{ text }}</div>
             </el-card>
 
             <el-card class="jugehappen">
               <div slot="header" class="jugehappentop">
                 <span>生态环境危害分析</span>
               </div>
-              <el-input class="inputtype" type="textarea" :autosize="{ minRows: 6, maxRows: 10}" placeholder="请输入内容" v-model="textarea4"></el-input>
+              <!-- <el-input class="inputtype" type="textarea" :autosize="{ minRows: 6, maxRows: 10}" placeholder="请输入内容" v-model="textarea4"></el-input>
+               -->
+               <div v-for="(text, index) in text4" :key="index" style="margin-top: 15px">{{ index + 1 }}、{{ text }}</div>
             </el-card>
           </div>
           <div class="last_row">
@@ -53,7 +59,9 @@
               <div slot="header" class="jugehappentop">
                 <span>其他危害分析</span>
               </div>
-              <el-input class="inputtype" type="textarea" :autosize="{ minRows: 6, maxRows: 10}" placeholder="请输入内容" v-model="textarea5"></el-input>
+              <!-- <el-input class="inputtype" type="textarea" :autosize="{ minRows: 6, maxRows: 10}" placeholder="请输入内容" v-model="textarea5"></el-input> 
+              -->
+              <div v-for="(text, index) in text5" :key="index" style="margin-top: 15px">{{ index + 1 }}、{{ text }}</div>
             </el-card>
           </div>
         </div>
@@ -124,9 +132,30 @@ const textarea3 = ref("");
 const textarea4 = ref("");
 const textarea5 = ref("");
 const text = ref("");
+const text1 = ref([
+  "风险事件的历史数据：通过分析过去发生的类似风险事件的频率和规模，判断此次风险发生的概率为65%。",
+  "相关因素的影响：分析与风险事件相关的各种因素，包括外部环境、市场趋势、政策法规等，判断此次风险发生的概率为60%。",
+  "现行控制措施的有效性：评估当前已经实施的风险控制措施对减少风险事件发生的影响，判断此次风险发生的概率为50%。",
+  "未来趋势和变化：考虑未来可能出现的变化和趋势，判断此次风险发生的概率为50%。"])
+const text2 = ref([
+  "化学危害：现场勘查可能涉及到接触有害化学物质，如有毒气体、腐蚀性物质等，可能引起中毒或化学灼伤。",
+  "身体伤害：在现场勘查中可能存在摔倒、碰撞、切割、烧伤等身体伤害风险，特别是在不安全的地形或环境中工作时更容易发生。",
+  "物理危害：现场勘查可能存在受到振动、噪音、辐射等物理因素的危害，长期暴露可能导致职业病。",
+  "生物危害：在某些环境中，现场勘查可能存在受到细菌、病毒、动植物等生物因素的危害，可能导致传染病或其他健康问题。",
+  "安全设施不全：现场勘查可能存在缺乏安全设施、不稳定的工作平台、不安全的设备等问题，增加了工作人员的受伤风险。"])
 
-
-const tableData1 = ref([{
+const text3 = ref([
+  "直接感染风险：如果生物安全事件涉及传染性疾病，附近居民可能面临直接感染的风险。需要评估病原体的传播途径、潜在感染人群以及可能的感染风险程度，以便采取相应的控制和预防措施，如隔离患病人群、提供疫苗接种等，以减少居民的感染风险。",
+  "粉尘、气味和污染物暴露：某些生物安全事件可能导致粉尘、气味或其他污染物的释放，对附近居民的呼吸道和健康造成影响。需要评估可能的暴露途径、暴露浓度和暴露时间，采取相应的控制措施，如限制居民进入受污染区域、提供口罩等防护用具，以减少居民的暴露风险。",
+  "心理健康影响：生物安全事件可能对附近居民的心理健康造成影响，特别是在面对疫情等紧急情况时。需要对居民的心理健康进行关注和支持，提供必要的心理健康教育和支持措施，以减少心理健康问题的发生。"])
+const text4 = ref([
+  "生物多样性损害：生物安全事件可能导致某些生物种群的大规模死亡或迁徙，对当地生态系统的物种多样性造成损害。需要评估受影响的生物种群以及可能的生态系统稳定性和功能损害，采取相应的保护措施，如野生动物救助和保护、生态系统修复等，以减少生物多样性损害。",
+  "土壤和水体污染：某些生物安全事件可能导致有害物质的释放，对土壤和水体造成污染。需要评估可能的污染物种类、浓度和扩散范围，采取相应的污染防控和修复措施，如土壤修复、水体净化等，以减少土壤和水体污染对生态环境的影响。",
+  "生态系统破坏：生物安全事件可能导致生态系统结构和功能的破坏，如湿地退化、森林破坏等。需要评估可能的生态系统破坏程度和影响范围，采取相应的保护和修复措施，如生态系统保护区设立、生态系统重建等，以减少生态系统破坏对生态环境的影响。"])
+const text5 = ref([
+  "经济危害：生物安全事件可能对农业、畜牧业、渔业、林业等产业造成严重影响，导致经济损失。",
+  "动物危害：生物安全事件可能对家畜、野生动物和水生动物等造成危害，包括感染疾病、死亡、栖息地破坏等。"])
+  const tableData1 = ref([{
             id: 1,
             virue: '鼠疫',
             checked: false,
@@ -253,14 +282,14 @@ function selectFile() {
 }
 .box-card{
   margin-top: 10px;
-  height: 1000px;
+  height: 1500px;
 }
 
 .jugehappen{
-  height: 300px;
+  height: 600px;
 }
 .field_hazard{
-  height: 300px;
+  height: 600px;
 }
 .last_row{
   margin-left: 10px;
