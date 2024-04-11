e5c0acb091419d19072ddb1f34d54ae72395cea1
1159911026@qq.com <1159911026@qq.com>
2024-04-11 15:11:17 +0800

    更新碱基检测

--- src/components/rapidIdentification/basePairs.vue
+++ src/components/rapidIdentification/basePairs.vue
@@ -1,21 +1,28 @@
 <template>
-  <div style="width: 100%;">
+  <!-- <div style="width: 100%;">
     <label style="font:16px Extra large">提交数据</label>
-  </div>
+  </div> -->
   <div class="mainbody">
     <el-card class="box-card1">
-      <div class="text item">
+      <span style="font:16px Extra large ">提交数据</span>
+      <div class="text">
         <!-- 碱基输入 -->
-        <el-input type="textarea" :rows="5" placeholder="请输入碱基序列" v-model="textarea">
+        <el-input type="textarea" class="basepairsinput" :rows="10" placeholder="请输入碱基序列或者使用文件上传" v-model="textarea">
         </el-input>
         <!-- 文件上传 -->
-
+        <el-upload class="upload-demo" drag action="https://jsonplaceholder.typicode.com/posts/" multiple>
+          <i class="el-icon-upload"></i>
+          <div class="el-upload__text">将文件拖到此处，或<em>点击上传</em></div>
+          <div class="el-upload__tip" slot="tip">只能上传excel文件，且不超过500kb</div>
+        </el-upload>
 
       </div>
+      <el-button type="primary" @click="submitbasePairs">提交</el-button>
     </el-card>
     <el-card class="box-card1">
-      <div class="text item">
-
+      <span style="font:16px Extra large">碱基序列检测结果</span>
+      <div class="item">
+        <p>{{ basePairsresult }}</p>
       </div>
     </el-card>
   </div>
@@ -27,22 +34,30 @@ import { ref } from 'vue'
 
 const textarea = ref("")
 
+const basePairsresult = ref("这是肝癌碱基序列")
+
+function submitbasePairs() {
+
+}
+
 </script>
 
 
 <style scoped>
 .mainbody {
-  margin-top: 10px;
+  margin-top: 5px;
   display: flex;
 }
 
 .text {
+  margin-top: 5px;
   font-size: 14px;
 }
 
-/* .item {
-  padding: 18px 0;
-} */
+.item {
+  margin-top: 15px;
+  font-size: medium;
+}
 
 .box-card1 {
 
@@ -52,9 +67,7 @@ const textarea = ref("")
   flex: 1;
 }
 
-/* .box-card2 {
-  margin-right: 10px;
-  height: 500px;
-  flex: 1;
-} */
+.basepairsinput {
+  margin-bottom: 20px;
+}
 </style>
